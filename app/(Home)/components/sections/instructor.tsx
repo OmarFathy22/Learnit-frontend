8f8b1ce8986b9477ff190404b4035eaac225262e
OmarFathy22 <of405789@gmail.com>
2024-04-20 05:12:22 +0200

    finish v1 of homePage

--- app/(Home)/components/sections/instructor.tsx
+++ app/(Home)/components/sections/instructor.tsx
@@ -6,22 +6,22 @@ export interface IAppProps {}
 
 export default function App(props: IAppProps) {
   return (
-    <div className="bg-[--bg-secondary] py-[30px] flex flex-col items-center gradient-t-border  ">
+    <div className="bg-[--bg-secondary] py-[30px] pb-[70px] flex flex-col items-center gradient-t-border  ">
       {/* react slider Here */}
       <div style={{
-        backgroundImage: `url('/become_instructor.png')`,
-        backgroundSize: "contain",
+        backgroundImage: `url('/instructor_new.png')`,
+        backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
       }}
-          className="global-padding  w-full h-[300px]">
+          className="global-padding   w-[70%] h-[400px] ">
       
       </div>
-    <div className="flex flex-col gap-3 justify-start items-center mt-[30px]">
-        <h1 className="gradient-text text-[30px]   ">
+    <div className="flex flex-col gap-3 justify-start items-center mt-[30px]  w-[70%]">
+        <h1 className="gradient-text text-[30px]  text-center ">
         Become an instructor
         </h1>
-        <h1 className="mb-3">Join instructors globally in educating millions on LearnIt. Empower your teaching passion with our tools and expertise.</h1>
+        <h1 className="mb-3 text-center">Join instructors globally in educating millions on LearnIt. Empower your teaching passion with our tools and expertise.</h1>
         <Link href="/instructor" className="gradient-bg p-2 px-[50px] rounded-md">Start teaching today </Link>
     </div>
     </div>
