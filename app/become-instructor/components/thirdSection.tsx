cc7f0fdbcc48b5336c8eb70cc8d385c2520e156e
morata222 <of2222002@gmai.com>
2024-05-04 18:17:46 +0300

    adding join us button

--- app/become-instructor/components/thirdSection.tsx
+++ app/become-instructor/components/thirdSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import Select from "react-select";
 export interface IAppProps {
-  selectStyles : any
+  selectStyles: any;
 }
 const countryOptions = [
   { value: "Egypt", label: "Egypt" },
@@ -11,54 +11,51 @@ const countryOptions = [
   { value: "United Arab Emirates", label: "United Arab Emirates" },
 ];
 const fieldOptions = [
-   { value: "Software Development", label: "Software Development" },
+  { value: "Software Development", label: "Software Development" },
   { value: "Business", label: "Business" },
   { value: "Design", label: "Design" },
   { value: "Personal Development", label: "Personal Development" },
 ];
 
-
 export interface IAppProps {}
 
-export default function App({selectStyles}: IAppProps) {
-
-  
+export default function App({ selectStyles }: IAppProps) {
   return (
     <div className="bg-[--bg-secondary] rounded-md  p-[30px] mt-2  flex flex-col items-center gap-5">
-        <div className="w-full">
+      <div className="w-full">
         <label htmlFor="displayName" className="mb-1 block">
           Diplay Name
         </label>
         <input
-        id="displayName"
-        placeholder="This is the name that will be visible to others on the platform."
-        type="text"
-        className="w-full bg-transparent border border-[#46464C] rounded-sm py-3 px-1 mt-1 "
-      />
+          id="displayName"
+          placeholder="This is the name that will be visible to others on the platform."
+          type="text"
+          className="w-full bg-transparent border border-[#46464C] rounded-sm py-3 px-1 mt-1 "
+        />
       </div>
 
-        <div className="w-full">
+      <div className="w-full">
         <label htmlFor="instructor-email" className="mb-1 block">
-        Email Address
+          Email Address
         </label>
         <input
-        id="instructor-email"
-        placeholder="ex. example@email.com"
-        type="email"
-        className="w-full bg-transparent border border-[#46464C] rounded-sm py-3 px-1 mt-1 "
-      />
+          id="instructor-email"
+          placeholder="ex. example@email.com"
+          type="email"
+          className="w-full bg-transparent border border-[#46464C] rounded-sm py-3 px-1 mt-1 "
+        />
       </div>
 
       <div className="flex items-center gap-3 w-full ">
-          <div className="flex-1">
+        <div className="flex-1">
           <label htmlFor="instructor-country" className="mb-1 block">
-          Country
+            Country
           </label>
           <Select options={countryOptions} styles={selectStyles} />
         </div>
-          <div className="flex-1">
+        <div className="flex-1">
           <label htmlFor="instructor-field" className="mb-1 block">
-          Field
+            Field
           </label>
           <Select options={fieldOptions} styles={selectStyles} />
         </div>
@@ -69,11 +66,15 @@ export default function App({selectStyles}: IAppProps) {
           Experience
         </label>
         <textarea
-        id="instructor-experience"
-        placeholder="give us a Brief summary of teaching or relevant experience"
-        className="min-h-[150px] w-full bg-transparent border border-[#46464C] rounded-sm py-3 px-1 mt-1 "
-      />
+          id="instructor-experience"
+          placeholder="give us a Brief summary of teaching or relevant experience"
+          className="min-h-[150px] w-full bg-transparent border border-[#46464C] rounded-sm py-3 px-1 mt-1 "
+        />
       </div>
+
+      <button className="w-full gradient-bg px-10 py-3 rounded-md font-bold hover:opacity-[0.95] active:scale-[.95] transition-all">
+        Join Us
+      </button>
     </div>
   );
 }
