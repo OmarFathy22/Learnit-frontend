1ea8447929d7bf4e924d3be0a3766ad92ee452d2
OmarFathy22 <of405789@gmail.com>
2024-05-06 21:10:43 +0300

    update course page responisveness

--- app/courses/[id]/components/leftSide/video.tsx
+++ app/courses/[id]/components/leftSide/video.tsx
@@ -7,7 +7,7 @@ export interface IAppProps {
 export default function App ({url}: IAppProps) {
   return (
     <div className=''>
-      <iframe className='h-[calc(100vh-80px)] w-full' src={url} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe className='h-[calc(100vh-80px)] max-1000:h-[50vh] w-full' src={url} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
   );
 }
