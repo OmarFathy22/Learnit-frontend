d6494bc5e548f0c40512c90c1ed95d181ca5d362
OmarFathy22 <of405789@gmail.com>
2024-06-27 13:21:59 +0300

    update pwa setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
 
-import nextPWA from '@ducanh2912/next-pwa';
-
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -18,8 +20,14 @@ const nextConfig = {
   },
 };
 
-const withPWA = nextPWA({
-  dest: 'public',
-});
+const nextConfigFunction = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};
 
-export default withPWA(nextConfig);
+export default nextConfigFunction;
